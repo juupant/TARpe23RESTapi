71051b7ac51d20596e5ad4eefff824af17c3f39e
alanunt <158561478+juupant@users.noreply.github.com>
2025-02-05 18:10:50 +0200

    harjutus1

---
+++ TARpe23RESTapi/RestAPIloomisejuhend/Model/DataContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestAPIloomisejuhend.Model
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        {
+        }
+        public DbSet<Exercise> ExerciseList { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Exercise>().HasData(
+                new Exercise
+                {
+                    Id = 1,
+                    Title = "kätekõverdused",
+                    Description = "tavalised",
+                    Intensity = Exercise.ExerciseIntensity.Normal,
+                    RecommendedDurationInSeconds = 40,
+                    RecommendedTimeInSecondsAfterExercise = 10,
+                    RecommendedTimeInSecondsBeforeExercise = 10
+                },
+                new Exercise
+                {
+                    Id = 2,
+                    Title = "slaalomhüpped",
+                    Description = "kükid",
+                    Intensity = Exercise.ExerciseIntensity.High,
+                    RecommendedDurationInSeconds = 40,
+                    RecommendedTimeInSecondsBeforeExercise = 10,
+                    RecommendedTimeInSecondsAfterExercise = 10,
+                    RestTimeInstructions = "asd"
+                },
+                new Exercise
+                {
+                    Id = 3,
+                    Title = "alt läbi jooks",
+                    Description = "toenglamang",
+                    Intensity = Exercise.ExerciseIntensity.Normal,
+                    RecommendedDurationInSeconds = 40,
+                    RecommendedTimeInSecondsBeforeExercise = 10,
+                    RecommendedTimeInSecondsAfterExercise = 10
+                });
+
+        }
+    }
+}
